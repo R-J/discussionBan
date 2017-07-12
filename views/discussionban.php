9eaaf48b5455f83df57ef0e262b5ea5b31ce05b9
R-J <robin.jurinka@gmx.net>
2017-07-12 13:06:07 +0200

    Clean up view

--- views/discussionban.php
+++ views/discussionban.php
@@ -1,27 +1,25 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 <h1><?php echo $this->data('Title') ?></h1>
-<div class="">
-<?php
-echo $this->Form->open();
-echo $this->Form->errors();
-?>
+<div class="FormWrapper DiscussionBan">
+    <?php
+    echo $this->Form->open();
+    echo $this->Form->errors();
+    ?>
 
-<div class="P">
-   <?php
+    <ul>
+        <li>
+            <?php
+            echo $this->Form->label('Ban User', 'BanUser');
+            echo wrap(
+                $this->Form->textBox('BanUser', array('class' => 'InputBox MultiComplete')),
+                'div',
+                array('class' => 'TextBoxWrapper')
+            );
+            ?>
+        </li>
+    </ul>
 
-    echo '<div class="P">';
-    echo $this->Form->label('BanUser', 'Ban Users');
-    echo wrap($this->Form->textBox('To', array('MultiLine' => true, 'class' => 'MultiComplete')), 'div', array('class' => 'TextBoxWrapper'));
-    echo '</div>';
-
-   ?>
-</div>
-
-<?php
-echo '<div class="Buttons Buttons-Confirm">',
-    $this->Form->button(t('OK')), ' ',
-    $this->Form->button(t('Cancel'), array('type' => 'button', 'class' => 'Button Close')),
-    '</div>';
-echo $this->Form->close();
-?>
+    <?php
+    echo $this->Form->close('Ban Users');
+    ?>
 </div>
